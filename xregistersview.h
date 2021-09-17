1cf99c2c8fbdcab1aa6bbf98cd1bd178a8ef09d7
horsicq <horsicq@gmail.com>
2021-09-17 20:40:26 +0200

    Fix: 2021-09-17

--- xregistersview.h
+++ xregistersview.h
@@ -69,7 +69,7 @@ protected:
 
 protected:
     virtual void paintCell(QPainter *pPainter,qint32 nRow,qint32 nColumn,qint32 nLeft,qint32 nTop,qint32 nWidth,qint32 nHeight);
-    virtual void registerShortcuts(bool bState);
+    virtual void registerShortcuts(bool bState) override;
 
 private:
     QMap<QString, QVariant> g_mapRegisters;
